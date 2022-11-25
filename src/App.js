603862e6343dfc0de35b52908122085db769ee75
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-25 13:49:22 -0300

    feat: create route and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route to='/' element={ <Home /> } />
+      </Routes>
     </>
   );
 }
