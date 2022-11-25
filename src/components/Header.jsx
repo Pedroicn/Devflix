cad5e7b03b7d0b043e533f67273ab1b5071e605c
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-25 13:29:31 -0300

    feat: create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const NavBar = () => {
+const Header = () => {
   return (
-    <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
+    <header className='flex items-center justify-between p-4 z-[100] absolute w-full'>
       <h1 className='text-blue-600 text-4xl font-bold cursor-pointer'>DEVFLIX</h1>
       <div>
         <button className='text-white pr-4 text-xl'>Sign In</button>
         <button className='bg-blue-600 px-6 py-2 rounded cursor-pointer text-white text-xl'>Sign Up</button>
       </div>
-    </div>
+    </header>
   )
 }
 
-export default NavBar
+export default Header;
