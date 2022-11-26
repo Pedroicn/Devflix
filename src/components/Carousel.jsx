b9b99081fdfda3c5d85ec0c768fcb5c65b91d584
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-26 14:03:21 -0300

    feat: create the carousel component and receives props from the main component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Carousel = ({ title, fetchApi }) => {
+  
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Carousel from '../components/Carousel';
 import Main from '../components/Main';
+import requests from '../services/ApiTMDB';
 
 const Home = () => {
+
   return (
     <>
       <Main />
+      <Carousel title='Upcoming' fetchApi={ requests.requestUpcoming } />
+      <Carousel title='Popular' fetchApi={ requests.requestPopular } />
+      <Carousel title='Top RAted' fetchApi={ requests.requestTopRated } />
+      <Carousel title='Trending' fetchApi={ requests.requestTrending } />
+      <Carousel title='Horror' fetchApi={ requests.requestHorror } />
+      <Carousel title='Comedy' fetchApi={ requests.requestComedy } />
+      <Carousel title='Action' fetchApi={ requests.requestAction } />
+      <Carousel title='Drama' fetchApi={ requests.requestDrama } />
     </>
   )
 }
