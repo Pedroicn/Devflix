5423956f50d4b21ec617820463e1e308f15b6f3d
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-26 14:44:41 -0300

    feat: search for movies in the api by categories and put it on the screen

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Carousel = ({ title, fetchApi }) => {
-  
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      const response = await axios.get(fetchApi);
+      setMovies(response.data.results);
+    }
+    fetchPopularMovies();
+  }, [fetchApi]);
+  console.log(movies)
   return (
-    <div>Carousel</div>
+    <>
+      <h2 className='text-white font-bold md:text-xl p-4'>{ title }</h2>
+      <section className='realtive flex items-center'>
+        <div id={ 'slider' }>
+          {
+            movies.map((movie, index) => (
+              <div key={ index } className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+                <img src={`https://image.tmdb.org/t/p/w500/${movie?.backdrop_path}`} alt={ movie?.title } />
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
