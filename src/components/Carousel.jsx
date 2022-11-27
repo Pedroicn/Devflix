a249afe92f34c475ae570cf0aef9236980b9133b
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 14:45:45 -0300

    feat: add scroll function

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Movie from './Movie';
 
-const Carousel = ({ title, fetchApi }) => {
+const Carousel = ({ title, fetchApi, rowId }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,20 +13,30 @@ const Carousel = ({ title, fetchApi }) => {
     }
     fetchPopularMovies();
   }, [fetchApi]);
-  console.log(movies)
+
+  const slideLeft = () => {
+    let slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    let slider = document.getElementById('slider' + rowId);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  }
+
   return (
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{ title }</h2>
       <section className='realtive flex items-center'>
-        <MdChevronLeft className='bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 w-10 h-10 cursor-pointer z-10' size={ 40 } />
-        <div id={ 'slider' } className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+        <MdChevronLeft onClick={ slideLeft } className='bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10' size={ 40 } />
+        <div id={ 'slider' + rowId } className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {
             movies.map((movie, index) => (
               <Movie movie={ movie } key={ index } />
             ))
           }
         </div>
-        <MdChevronRight className='bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 w-10 h-10 cursor-pointer z-10' />
+        <MdChevronRight onClick={ slideRight } className='bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 cursor-pointer z-10' size={ 40 } />
       </section>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
   return (
     <>
       <Main />
-      <Carousel title='Upcoming' fetchApi={ requests.requestUpcoming } />
-      <Carousel title='Popular' fetchApi={ requests.requestPopular } />
-      <Carousel title='Top RAted' fetchApi={ requests.requestTopRated } />
-      <Carousel title='Trending' fetchApi={ requests.requestTrending } />
-      <Carousel title='Horror' fetchApi={ requests.requestHorror } />
-      <Carousel title='Comedy' fetchApi={ requests.requestComedy } />
-      <Carousel title='Action' fetchApi={ requests.requestAction } />
-      <Carousel title='Drama' fetchApi={ requests.requestDrama } />
+      <Carousel rowId='1' title='Upcoming' fetchApi={ requests.requestUpcoming } />
+      <Carousel rowId='2' title='Popular' fetchApi={ requests.requestPopular } />
+      <Carousel rowId='3' title='Top RAted' fetchApi={ requests.requestTopRated } />
+      <Carousel rowId='4' title='Trending' fetchApi={ requests.requestTrending } />
+      <Carousel rowId='5' title='Horror' fetchApi={ requests.requestHorror } />
+      <Carousel rowId='6' title='Comedy' fetchApi={ requests.requestComedy } />
+      <Carousel rowId='7' title='Action' fetchApi={ requests.requestAction } />
+      <Carousel rowId='8' title='Drama' fetchApi={ requests.requestDrama } />
     </>
   )
 }
