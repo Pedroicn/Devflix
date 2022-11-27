87fc8515cd33c412d26a04a128ddadbe2e26c590
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 16:31:01 -0300

    feat: add links to buttons corresponding to the created pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-      <h1 className='text-blue-600 text-4xl font-bold cursor-pointer'>DEVFLIX</h1>
+      <Link to='/'>
+        <h1 className='text-blue-600 text-4xl font-bold cursor-pointer'>DEVFLIX</h1>
+      </Link>
       <div>
-        <button className='text-white pr-4 text-xl'>Sign In</button>
-        <button className='bg-blue-600 px-6 py-2 rounded cursor-pointer text-white text-xl'>Sign Up</button>
+        <Link to='/login'>
+          <button className='text-white pr-4 text-xl'>Sign In</button>
+        </Link>
+        <Link to='/signup'>
+          <button className='bg-blue-600 px-6 py-2 rounded cursor-pointer text-white text-xl'>Sign Up</button>
+        </Link>
       </div>
     </header>
   )
