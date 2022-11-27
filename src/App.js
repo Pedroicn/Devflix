027702f4e5931a1124c9fe30a43f9252db48ba3b
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 16:24:17 -0300

    feat: create login, signup and account pages, and their routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider";
+import Login from "./pages/Login";
+import Account from './pages/Account';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={ <Home /> } />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/signup' element={ <SignUp /> } />
+          <Route path='/account' element={ <Account /> } />
         </Routes>
       </AuthProvider>
     </>
