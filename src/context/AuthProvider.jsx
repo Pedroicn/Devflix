befe002ad81fba33477b99c8c4d482c026f2c065
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 16:14:07 -0300

    feat: configure auth provider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AuthContext from './AuthContext'
+import auth from '../services/firebase'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  function signIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  function logout() {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unRegister = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    })
+    return () => unRegister();
+  }, [])
+
+  const contextValue = {
+    signUp,
+    signIn,
+    logout,
+    user
+  }
+
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={ contextValue }>
       { children }
     </AuthContext.Provider>
   )
