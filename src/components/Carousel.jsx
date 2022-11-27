fc788aa3e6ad5f38c7889a287cce9a349e57a3b9
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 14:08:08 -0300

    feat: add scroll function

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import Movie from './Movie';
 
 const Carousel = ({ title, fetchApi }) => {
@@ -18,13 +18,15 @@ const Carousel = ({ title, fetchApi }) => {
     <>
       <h2 className='text-white font-bold md:text-xl p-4'>{ title }</h2>
       <section className='realtive flex items-center'>
-        <div id={ 'slider' }>
+        <MdChevronLeft className='bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 w-10 h-10 cursor-pointer z-10' size={ 40 } />
+        <div id={ 'slider' } className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
           {
             movies.map((movie, index) => (
               <Movie movie={ movie } key={ index } />
             ))
           }
         </div>
+        <MdChevronRight className='bg-white rounded-full right-0 absolute opacity-50 hover:opacity-100 w-10 h-10 cursor-pointer z-10' />
       </section>
     </>
   )
