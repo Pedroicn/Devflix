9d9d1fa38180478859c2a1371fdbd1d924f4134b
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-27 15:54:05 -0300

    feat: create auth context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
 
 function App() {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/' element={ <Home /> } />
-      </Routes>
+      <AuthProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+        </Routes>
+      </AuthProvider>
     </>
   );
 }
