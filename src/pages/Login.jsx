048a147ab2ef1d6893dd0842871dbcfaf2d1eda1
Pedro Nunes <pedro.icn88@gmail.com>
2022-11-28 09:07:34 -0300

    feat: implement login logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {user, signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn(email, password);
+      navigate('/')
+    } catch (error) {
+      toast(error.message.slice(10), {
+        position: 'top-center',
+        theme: 'dark',
+        type: 'error',
+      })
+    }
+    
+  }
   return (
     <main className='w-full h-screen'>
       <img
@@ -14,10 +36,28 @@ const Login = () => {
         <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
           <section className='max-w-[320px] mx-auto py-16'>
             <h1 className='text-3xl font-bold'>Login</h1>
-            <form className='w-full flex flex-col py-4'>
-              <input className='p-3 my-2 bg-gray-700 rounded' type="email" placeholder='Email' autoComplete='email' />
-              <input className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Senha'  />
-              <button className='bg-blue-600 py-3 my-6 rounded font-bold' type='submit'>Entrar</button>
+            <form onSubmit={ handleSubmit } className='w-full flex flex-col py-4'>
+              <input
+                className='p-3 my-2 bg-gray-700 rounded'
+                type="email"
+                placeholder='Email'
+                autoComplete='email'
+                value={ email }
+                onChange={ ({ target }) => setEmail(target.value) }
+              />
+              <input
+                className='p-3 my-2 bg-gray-700 rounded'
+                type="password"
+                placeholder='Senha'
+                value={ password }
+                onChange={ ({ target }) => setPassword(target.value)}
+              />
+              <button
+                className='bg-blue-600 py-3 my-6 rounded font-bold'
+                type='submit'
+              >
+                Entrar
+              </button>
               <div className='flex justify-between items-center text-gray-400'>
                 <p><input className='mr-2' type="checkbox" />Lembre-me</p>
                 <p>Ajuda?</p>
